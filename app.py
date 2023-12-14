853a348cb7851f54c564e77250e73f3edb3f9fc6
Jonas <jonas@jonasonline.se>
2023-12-14 23:50:04 +0100

    Adding NVD api key support

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import requests
 import pandas as pd
 import gzip
 import io
+import json
+import time
+
+# Load configuration
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+# API configuration
+nvd_api_key = config['nvd_api_key']
+nvd_base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId="
+headers = {'apiKey': nvd_api_key}
 
 # URL for the EPSS feed
 epss_url = "https://epss.cyentia.com/epss_scores-current.csv.gz"
-nvd_base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId="
 
 # Function to download and extract the EPSS feed
 def download_and_extract_epss(url):
@@ -20,8 +30,7 @@ def download_and_extract_epss(url):
 
 # Function to fetch more information from NVD's database
 def fetch_nvd_data(cve_id):
-    response = requests.get(nvd_base_url + cve_id)
-    print(nvd_base_url + cve_id)
+    response = requests.get(nvd_base_url + cve_id, headers=headers)
     if response.status_code == 200:
         return response.json()
     else:
@@ -31,8 +40,7 @@ def fetch_nvd_data(cve_id):
 epss_df = download_and_extract_epss(epss_url)
 
 # Filter for CVEs with EPSS score greater than 0.5 and year 2015 or later
-high_risk_cves = epss_df[(epss_df['epss'] > 0.5) & (epss_df['cve'].str.contains('CVE-201[8-9]|CVE-20[2-9]'))]
-print(high_risk_cves.count)
+high_risk_cves = epss_df[(epss_df['epss'] > 0.5) & (epss_df['cve'].str.contains('CVE-201[5-9]|CVE-20[2-9]'))]
 
 # Fetch NVD data for the first 20 high-risk CVEs from 2015 or later
 nvd_data = []
@@ -40,6 +48,7 @@ for cve_id in high_risk_cves['cve'].head(20):
     try:
         nvd_info = fetch_nvd_data(cve_id)
         nvd_data.append(nvd_info)
+        time.sleep(1)  # Wait for 1 second before making the next request
     except Exception as e:
         print(e)
 
