400a21da72e2f7ea6ca4e5438ec1faa26c308abe
Jonas <jonas@jonasonline.se>
2023-12-18 23:13:38 +0100

    Adding manufacturer to output

--- app.py
+++ app.py
@@ -4,15 +4,37 @@ import gzip
 import io
 import json
 import time
+import os
+import re
 
 # Load configuration
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
-# API configuration
+# API and Teams configuration
 nvd_api_key = config['nvd_api_key']
+teams_webhook_url = config.get('teams_webhook_url')  # Get webhook URL if exists
+significant_increase_percent = config.get('significant_increase_percent', 20) / 100.0
 nvd_base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId="
 headers = {'apiKey': nvd_api_key}
+
+# Function to post messages to Microsoft Teams
+def post_to_teams(webhook_url, title, message):
+    if webhook_url:
+        payload = {
+            "@type": "MessageCard",
+            "@context": "http://schema.org/extensions",
+            "themeColor": "0076D7",
+            "title": title,
+            "text": message
+        }
+        response = requests.post(webhook_url, json=payload, headers={'Content-Type': 'application/json'})
+        if response.status_code == 200:
+            print("Message posted to Teams")
+        else:
+            print(f"Failed to post message to Teams: {response.status_code}")
+    else:
+        print("No Teams webhook URL provided in config.json, skipping post to Teams.")
 
 # URL for the EPSS feed
 epss_url = "https://epss.cyentia.com/epss_scores-current.csv.gz"
@@ -22,31 +44,59 @@ def download_and_extract_epss(url):
     response = requests.get(url)
     if response.status_code == 200:
         with gzip.open(io.BytesIO(response.content), 'rt') as f:
-            # Skip the first row and set the header to the second row
             df = pd.read_csv(f, header=1)
         return df
     else:
         raise Exception(f"Failed to download file: Status code {response.status_code}")
 
-# Function to fetch more information from NVD's database and process the response
-def fetch_and_process_nvd_data(cve_id):
+# Function to fetch more information from NVD's database
+def fetch_nvd_data(cve_id):
     response = requests.get(nvd_base_url + cve_id, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        process_nvd_response(data)
+        return response.json()
     else:
         raise Exception(f"Failed to fetch data for {cve_id}: Status code {response.status_code}")
 
-# Function to process NVD response
-def process_nvd_response(data):
-    for vuln in data.get('vulnerabilities', []):
-        cve_id = vuln.get('cve', {}).get('id', 'Unknown CVE ID')
-        descriptions = vuln.get('cve', {}).get('descriptions', [])
-        for desc in descriptions:
-            if desc.get('lang', '') == 'en':
-                print(f"CVE ID: {cve_id}")
-                print(f"Description: {desc.get('value', 'No description available')}\n")
-                break
+# Function to extract manufacturer from NVD data
+def extract_manufacturer(nvd_data):
+    manufacturers = set()
+
+    nvd_text = json.dumps(nvd_data)
+    criteria_matches = re.findall(r'"criteria":\s*"([^"]+)"', nvd_text)
+    
+    for criteria in criteria_matches:
+        parts = criteria.split(':')
+        if len(parts) > 3:
+            manufacturers.add(parts[3])
+
+    return list(manufacturers)
+
+# Function to check if a CVE should be notified
+def should_notify(cve_id, new_epss_score, existing_results):
+    for result in existing_results:
+        if result['cve_id'] == cve_id:
+            old_epss_score = result['epss_score']
+            increase = (new_epss_score - old_epss_score) / old_epss_score
+            return increase >= significant_increase_percent
+    return True  # Notify if it's a new CVE
+
+# Function to load existing results from "cloud storage"
+def load_existing_results(filename="nvd_results.json"):
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            return json.load(file)
+    return []
+
+# Function to simulate saving to cloud storage
+def save_to_cloud_storage(data, filename="nvd_results.json"):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+# Load existing results
+existing_results = load_existing_results()
+
+# Flag to determine if it's the first run (no previous results)
+is_first_run = len(existing_results) == 0
 
 # Download and extract the EPSS feed
 epss_df = download_and_extract_epss(epss_url)
@@ -54,10 +104,19 @@ epss_df = download_and_extract_epss(epss_url)
 # Filter for CVEs with EPSS score greater than 0.5 and year 2015 or later
 high_risk_cves = epss_df[(epss_df['epss'] > 0.5) & (epss_df['cve'].str.contains('CVE-201[5-9]|CVE-20[2-9]'))]
 
-# Fetch NVD data for the first 20 high-risk CVEs from 2015 or later and process each response
-for cve_id in high_risk_cves['cve'].head(20):
+# Fetch NVD data for the first 20 high-risk CVEs from 2015 or later
+all_results = []
+for cve_id, epss_score in zip(high_risk_cves['cve'].head(20), high_risk_cves['epss'].head(20)):
     try:
-        fetch_and_process_nvd_data(cve_id)
-        time.sleep(1)  # Rate limiting: one request per second
+        nvd_data = fetch_nvd_data(cve_id)
+        manufacturers = extract_manufacturer(nvd_data)
+        # Only notify if it's not the first run and the conditions are met
+        if not is_first_run and should_notify(cve_id, epss_score, existing_results):
+            post_to_teams(teams_webhook_url, f"Update for {cve_id}", f"EPSS Score: {epss_score}, Manufacturers: {', '.join(manufacturers)}")
+        all_results.append({"cve_id": cve_id, "epss_score": epss_score, "manufacturers": manufacturers})
+        time.sleep(1)  # Rate limiting
     except Exception as e:
         print(e)
+
+# Save all new and updated results
+save_to_cloud_storage(all_results)
